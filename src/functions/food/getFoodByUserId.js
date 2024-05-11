85124b469417903f8bdd131186697dcd2bee5a56
Hunter Long <whunterlong@gmail.com>
2024-05-11 13:24:17 -0400

    Update getFoodByUserId response

--- src/functions/food/getFoodByUserId.js
+++ src/functions/food/getFoodByUserId.js
@@ -1,34 +1,14 @@
-// import AWS from "aws-sdk";
 import dynamodb from "../../db/dynamodbClient.js";
 
-const getFoodByUserId = async (event) => {
-  // const dynamodb = new AWS.DynamoDB.DocumentClient();
-  
+const getFoodByUserId = async (event) => {  
   try {
-    
     const { userId } = event.requestContext.authorizer;
-    // const queryKey = "userId";
-    // const queryValue = email;
     const res = await dynamodb.queryByGSI("FoodTable", "UserIdIndex", "userId", userId)
-
-
-    console.log("RESSSSS",res)
-
-    // const result = await dynamodb.get({
-    //   TableName: "FoodTable",
-    //   // Key: { userId }
-    // }).promise();
-
-
-    // const params = {
-    //   TableName: 
-    // }
-    // const res = await dynamodb.query(params).promise()
-
+    const foodItems = res.Items
     return {
       statusCode: 200,
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({message: "ok"}),
+      body: JSON.stringify(foodItems),
     };
   } catch (error) {
     console.error("DynamoDB Error:", error);
